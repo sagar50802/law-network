3ab3a0172a437275298338396490979f3c3c3cd5
sagar5080 <sagartripathi62666@gmail.com>
2025-09-24 19:35:46 +0530

    Fix Article model: convert to ESM export

--- server/models/Article.js
+++ server/models/Article.js
@@ -6,7 +6,7 @@ const articleSchema = new mongoose.Schema(
     title: { type: String, required: true },
     content: { type: String, required: true }, // full text
     link: { type: String, default: "" },
-    image: { type: String, default: "" }, // /uploads/articles/filename.jpg
+    image: { type: String, default: "" },      // /uploads/articles/filename.jpg
     allowHtml: { type: Boolean, default: false },
     isFree: { type: Boolean, default: false },
   },
@@ -14,5 +14,4 @@ const articleSchema = new mongoose.Schema(
 );
 
 const Article = mongoose.model("Article", articleSchema);
-
 export default Article;
