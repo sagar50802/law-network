1c40212758b69147bb453c98a1cc5be028bcfd38
sagar50802 <sagartripathi1311@gmail.com>
2025-10-03 03:23:56 +0530

    Update Playlist.js

--- server/models/Playlist.js
+++ server/models/Playlist.js
@@ -1,16 +1,8 @@
-// server/models/Playlist.js
 import mongoose from "mongoose";
-
-const itemSchema = new mongoose.Schema({
-  title: String,
-  artist: String,
-  url: String,
-  locked: Boolean,
-});
 
 const playlistSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  items: [itemSchema],
+  createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model("Playlist", playlistSchema);
