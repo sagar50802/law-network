b478c279b81779a95b8a4a27756d049596aa4a65
sagar50802 <sagartripathi1311@gmail.com>
2025-10-03 14:48:28 +0530

    Update PlaylistWrapper.js

--- server/models/PlaylistWrapper.js
+++ server/models/PlaylistWrapper.js
@@ -2,33 +2,10 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
-function loadCjs(paths) {
-  for (const p of paths) {
-    try {
-      return require(p);
-    } catch {}
-  }
-  return null;
-}
+// Your existing CommonJS Mongoose model is in the SAME folder:
+const cjs = require("./Playlist.js");
 
-/**
- * Try these common locations:
- *  1) ./Playlist.js                 (server/models/Playlist.js)
- *  2) ../models/Playlist.js         (server/models from a sibling)
- *  3) ../../models/Playlist.js      (repo-root/models/Playlist.js)
- */
-const cjs = loadCjs([
-  "./Playlist.js",
-  "../models/Playlist.js",
-  "../../models/Playlist.js",
-]);
-
-if (!cjs) {
-  throw new Error(
-    "Cannot locate models/Playlist.js. Tried ./Playlist.js, ../models/Playlist.js, ../../models/Playlist.js"
-  );
-}
-
-const Model = cjs?.default || cjs; // support either export style
+// If it's CommonJS, require() returns the model; if ESM compiled to CJS, it may be on .default
+const Model = (cjs && cjs.default) || cjs;
 
 export default Model;
