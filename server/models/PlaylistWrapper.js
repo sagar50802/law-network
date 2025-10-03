8712d48e2f4cae6beb0b2491efca288c2a0f903b
sagar50802 <sagartripathi1311@gmail.com>
2025-10-03 14:15:42 +0530

    Update PlaylistWrapper.js

--- server/models/PlaylistWrapper.js
+++ server/models/PlaylistWrapper.js
@@ -2,10 +2,33 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
-// Load your existing CommonJS mongoose model
-const cjsModel = require("./Playlist.js");
+function loadCjs(paths) {
+  for (const p of paths) {
+    try {
+      return require(p);
+    } catch {}
+  }
+  return null;
+}
 
-// Support either `module.exports = Model` or `{ default: Model }`
-const Model = cjsModel?.default || cjsModel;
+/**
+ * Try these common locations:
+ *  1) ./Playlist.js                 (server/models/Playlist.js)
+ *  2) ../models/Playlist.js         (server/models from a sibling)
+ *  3) ../../models/Playlist.js      (repo-root/models/Playlist.js)
+ */
+const cjs = loadCjs([
+  "./Playlist.js",
+  "../models/Playlist.js",
+  "../../models/Playlist.js",
+]);
+
+if (!cjs) {
+  throw new Error(
+    "Cannot locate models/Playlist.js. Tried ./Playlist.js, ../models/Playlist.js, ../../models/Playlist.js"
+  );
+}
+
+const Model = cjs?.default || cjs; // support either export style
 
 export default Model;
