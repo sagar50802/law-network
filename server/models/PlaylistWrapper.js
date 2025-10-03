ac24a3a4a730f8a9ed06bc034f9bc814dd6e1fbc
sagar50802 <sagartripathi1311@gmail.com>
2025-10-03 13:31:44 +0530

    Create PlaylistWrapper.js

---
+++ server/models/PlaylistWrapper.js
@@ -0,0 +1,3 @@
+// server/models/PlaylistWrapper.js
+import cjsPlaylist from "./Playlist.js"; // your existing CommonJS model
+export default cjsPlaylist;               // expose it as a default export for ESM
