33f56e62943630ae36870fd5ade9d2699efd827d
sagar50802 <sagartripathi1311@gmail.com>
2025-10-03 14:47:39 +0530

    Update isOwnerWrapper.js

--- server/middlewares/isOwnerWrapper.js
+++ server/middlewares/isOwnerWrapper.js
@@ -2,34 +2,11 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
-function loadCjs(paths) {
-  for (const p of paths) {
-    try {
-      return require(p);
-    } catch {}
-  }
-  return null;
-}
+// Your existing CommonJS middleware is in the SAME folder:
+const cjs = require("./isOwner.js");
 
-/**
- * Try these, in order:
- *  1) ../../middlewares/isOwner.js         (repo-root/middlewares)
- *  2) ./isOwner.js                         (server/middlewares)
- *  3) ../middlewares/isOwner.js            (server/middlewares from nested callsites)
- */
-const cjs = loadCjs([
-  "../../middlewares/isOwner.js",
-  "./isOwner.js",
-  "../middlewares/isOwner.js",
-]);
-
-if (!cjs) {
-  throw new Error(
-    "Cannot locate middlewares/isOwner.js. Tried ../../middlewares/isOwner.js, ./isOwner.js, ../middlewares/isOwner.js"
-  );
-}
-
-const isOwner = typeof cjs === "function" ? cjs : cjs.default;
+// If the CJS module exports the function directly, use it; otherwise use .default
+const isOwner = typeof cjs === "function" ? cjs : (cjs && cjs.default);
 
 export default function isOwnerWrapper(req, res, next) {
   return isOwner(req, res, next);
