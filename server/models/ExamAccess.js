b73892f5c2f3359f5af47887c9ee8f24521f23e1
sagar50802 <sagartripathi1311@gmail.com>
2025-10-12 02:32:34 +0530

    Update ExamAccess.js

--- server/models/ExamAccess.js
+++ server/models/ExamAccess.js
@@ -1,15 +1,2 @@
-import mongoose from "mongoose";
-
-const ExamAccessSchema = new mongoose.Schema({
-  userEmail: { type: String, index: true, required: true },
-  examId:    { type: String, index: true, required: true },
-  planDays:  { type: Number, required: true },
-  startAt:   { type: Date,   required: true },
-  expiryAt:  { type: Date,   required: true },
-  status:    { type: String, enum: ["active","archived"], default: "active" },
-}, { timestamps: true });
-
-ExamAccessSchema.index({ userEmail:1, examId:1, status:1 });
-
-export default mongoose.models.ExamAccess ||
-  mongoose.model("ExamAccess", ExamAccessSchema);
+// models/ExamAccess.js
+export { default } from "./PrepAccess.js";
