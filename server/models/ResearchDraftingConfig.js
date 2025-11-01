b28810dc0ef377d6c1c1ffc10443f4d7468bdf86
sagar50802 <sagartripathi1311@gmail.com>
2025-11-01 20:20:10 +0530

    Update ResearchDraftingConfig.js

--- server/models/ResearchDraftingConfig.js
+++ server/models/ResearchDraftingConfig.js
@@ -1,11 +1,12 @@
 // server/models/ResearchDraftingConfig.js
 import mongoose from "mongoose";
 
-const ResearchDraftingConfigSchema = new mongoose.Schema({
+const schema = new mongoose.Schema({
   key: { type: String, unique: true }, // "rd:config"
   upiId: { type: String, default: "" },
   defaultAmount: { type: Number, default: 299 },
-  waNumber: { type: String, default: "" }, // "91XXXXXXXXXX" (no +)
+  waNumber: { type: String, default: "" },
 }, { timestamps: true });
 
-export default mongoose.model("ResearchDraftingConfig", ResearchDraftingConfigSchema);
+export default mongoose.models.ResearchDraftingConfig ||
+       mongoose.model("ResearchDraftingConfig", schema);
