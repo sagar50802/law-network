2dd8f787a324a7dd752bb19149f74f06dcde359f
sagar50802 <sagartripathi1311@gmail.com>
2025-11-15 16:23:09 +0530

    Delete server/utils/catchAsync.js

--- server/utils/catchAsync.js
+++
@@ -1,8 +0,0 @@
-export default function catchAsync(fn) {
-  return function (req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch((err) => {
-      console.error("API ERROR:", err);
-      res.status(500).json({ ok: false, error: err.message });
-    });
-  };
-}
