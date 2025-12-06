e44200ab59fde7bef06e872813d931c8458692f7
sagar50802 <sagartripathi1311@gmail.com>
2025-12-06 13:26:51 +0530

    Create Exam.js

---
+++ server/answerWriting/models/Exam.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const examSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Exam", examSchema);
