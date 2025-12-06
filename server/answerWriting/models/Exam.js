d8f534e2833bf0253b29375f4512aeff6ea50b8a
sagar50802 <sagartripathi1311@gmail.com>
2025-12-06 19:15:11 +0530

    Update Exam.js

--- server/answerWriting/models/Exam.js
+++ server/answerWriting/models/Exam.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const examSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
+    // e.g. "bihar-apo", "up-apo"
+    code: { type: String, required: true, unique: true },
   },
   { timestamps: true }
 );
