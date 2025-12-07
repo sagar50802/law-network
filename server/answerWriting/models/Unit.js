93ff77c532f04bd016ef136563f157062865b4a3
sagar50802 <sagartripathi1311@gmail.com>
2025-12-07 08:46:18 +0530

    Update Unit.js

--- server/answerWriting/models/Unit.js
+++ server/answerWriting/models/Unit.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
-const UnitSchema = new mongoose.Schema({
-  examId: { type: mongoose.Schema.Types.ObjectId, ref: "Exam", required: true },
-  name: { type: String, required: true },
-  locked: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
-});
+const unitSchema = new mongoose.Schema(
+  {
+    examId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Exam",
+      required: true,
+    },
+    name: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.models.Unit || mongoose.model("Unit", UnitSchema);
+// Prevent OverwriteModelError
+export default mongoose.models.Unit || mongoose.model("Unit", unitSchema);
