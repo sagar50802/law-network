c61ed795a76d91283201f96851214a30a63b702a
sagar50802 <sagartripathi1311@gmail.com>
2025-12-07 08:22:04 +0530

    Update Question.js

--- server/answerWriting/models/Question.js
+++ server/answerWriting/models/Question.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 
 const questionSchema = new mongoose.Schema(
   {
-    // hierarchy
     examId: { type: mongoose.Schema.Types.ObjectId, ref: "Exam" },
     unitId: { type: mongoose.Schema.Types.ObjectId, ref: "Unit" },
     topicId: { type: mongoose.Schema.Types.ObjectId, ref: "Topic" },
@@ -13,16 +12,13 @@ const questionSchema = new mongoose.Schema(
       required: true,
     },
 
-    // content
     hindiText: { type: String },
     englishText: { type: String },
 
-    // scheduling
     releaseAt: { type: Date, required: true },
     isReleased: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
 
-const Question = mongoose.model("Question", questionSchema);
-export default Question;
+export default mongoose.models.Question || mongoose.model("Question", questionSchema);
