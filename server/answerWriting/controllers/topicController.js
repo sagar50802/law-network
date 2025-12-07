0806be864a8b3f51fbeebd60014b14222d5026ed
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 00:09:18 +0530

    Update topicController.js

--- server/answerWriting/controllers/topicController.js
+++ server/answerWriting/controllers/topicController.js
@@ -1,67 +1,42 @@
 import Topic from "../models/Topic.js";
-import Subtopic from "../models/Subtopic.js";
-import Question from "../models/Question.js";
+import Unit from "../models/Unit.js";
 
-export async function createTopic(req, res) {
+export const createTopic = async (req, res) => {
   try {
     const topic = await Topic.create({
-      unitId: req.params.unitId,
+      unit: req.params.unitId,
       name: req.body.name,
     });
 
-    return res.json({ success: true, topic });
-  } catch (err) {
-    console.error("createTopic error:", err);
-    return res.status(500).json({ success: false, message: err.message });
-  }
-}
+    await Unit.findByIdAndUpdate(req.params.unitId, {
+      $push: { topics: topic._id },
+    });
 
-export async function toggleLock(req, res) {
+    res.json({ success: true, topic });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+export const updateTopic = async (req, res) => {
   try {
-    const updated = await Topic.findByIdAndUpdate(
+    const topic = await Topic.findByIdAndUpdate(
       req.params.topicId,
-      { locked: !!req.body.locked },
+      { name: req.body.name },
       { new: true }
     );
 
-    return res.json({ success: true, topic: updated });
+    res.json({ success: true, topic });
   } catch (err) {
-    console.error("toggleLock error:", err);
-    return res.status(500).json({ success: false, message: err.message });
+    res.status(500).json({ success: false, message: err.message });
   }
-}
+};
 
-export async function updateTopic(req, res) {
+export const deleteTopic = async (req, res) => {
   try {
-    const updated = await Topic.findByIdAndUpdate(
-      req.params.topicId,
-      req.body,
-      { new: true }
-    );
-
-    return res.json({ success: true, topic: updated });
+    await Topic.findByIdAndDelete(req.params.topicId);
+    res.json({ success: true });
   } catch (err) {
-    console.error("updateTopic error:", err);
-    return res.status(500).json({ success: false, message: err.message });
+    res.status(500).json({ success: false, message: err.message });
   }
-}
-
-export async function deleteTopic(req, res) {
-  try {
-    const { topicId } = req.params;
-
-    const subs = await Subtopic.find({ topicId });
-
-    for (const s of subs) {
-      await Question.deleteMany({ subtopicId: s._id });
-    }
-
-    await Subtopic.deleteMany({ topicId });
-    await Topic.findByIdAndDelete(topicId);
-
-    return res.json({ success: true, message: "Topic deleted" });
-  } catch (err) {
-    console.error("deleteTopic error:", err);
-    return res.status(500).json({ success: false, message: err.message });
-  }
-}
+};
