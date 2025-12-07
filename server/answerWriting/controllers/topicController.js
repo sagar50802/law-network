e1598947c11c743a106eefa2ce68859344a7cf87
sagar50802 <sagartripathi1311@gmail.com>
2025-12-07 13:55:15 +0530

    Update topicController.js

--- server/answerWriting/controllers/topicController.js
+++ server/answerWriting/controllers/topicController.js
@@ -1,4 +1,3 @@
-// server/answerWriting/controllers/topicController.js
 import Topic from "../models/Topic.js";
 import Subtopic from "../models/Subtopic.js";
 import Question from "../models/Question.js";
@@ -7,7 +6,7 @@ export async function createTopic(req, res) {
   try {
     const topic = await Topic.create({
       unitId: req.params.unitId,
-      name: req.body.name,
+      name: req.body.name
     });
 
     res.json({ success: true, topic });
@@ -35,7 +34,6 @@ export async function deleteTopic(req, res) {
     const { topicId } = req.params;
 
     const subs = await Subtopic.find({ topicId });
-
     for (const s of subs) {
       await Question.deleteMany({ subtopicId: s._id });
     }
@@ -43,7 +41,7 @@ export async function deleteTopic(req, res) {
     await Subtopic.deleteMany({ topicId });
     await Topic.findByIdAndDelete(topicId);
 
-    res.json({ success: true, message: "Topic deleted" });
+    res.json({ success: true });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
   }
@@ -51,11 +49,7 @@ export async function deleteTopic(req, res) {
 
 export async function updateTopic(req, res) {
   try {
-    const updated = await Topic.findByIdAndUpdate(
-      req.params.topicId,
-      req.body,
-      { new: true }
-    );
+    const updated = await Topic.findByIdAndUpdate(req.params.topicId, req.body, { new: true });
 
     res.json({ success: true, topic: updated });
   } catch (err) {
