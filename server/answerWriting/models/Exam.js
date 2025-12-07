2ca0ac9fc116d0a2d026d2b239e3a9ae57de1cb7
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 00:38:33 +0530

    Update Exam.js

--- server/answerWriting/models/Exam.js
+++ server/answerWriting/models/Exam.js
@@ -3,11 +3,9 @@ import mongoose from "mongoose";
 const ExamSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    units: [{ type: mongoose.Schema.Types.ObjectId, ref: "Unit" }],
   },
   { timestamps: true }
 );
 
-// SAFE EXPORT → prevents OverwriteModelError
-export default mongoose.models.Exam ||
-  mongoose.model("Exam", ExamSchema);
+// Prevent OverwriteModelError
+export default mongoose.models.Exam || mongoose.model("Exam", ExamSchema);
