c99e4b924dd388b6c5b42edec296abb2d946e7ed
sagar50802 <sagartripathi1311@gmail.com>
2025-12-07 13:54:52 +0530

    Update subtopicController.js

--- server/answerWriting/controllers/subtopicController.js
+++ server/answerWriting/controllers/subtopicController.js
@@ -1,13 +1,12 @@
-// server/answerWriting/controllers/subtopicController.js
 import Subtopic from "../models/Subtopic.js";
 import Question from "../models/Question.js";
 
 export async function createSubtopic(req, res) {
   try {
-    const subtopic = await Subtopic.create({
-      topicId: req.params.topicId,
-      name: req.body.name,
-    });
+    const { topicId } = req.params;
+    const { name } = req.body;
+
+    const subtopic = await Subtopic.create({ topicId, name });
 
     res.json({ success: true, subtopic });
   } catch (err) {
@@ -22,7 +21,7 @@ export async function deleteSubtopic(req, res) {
     await Question.deleteMany({ subtopicId });
     await Subtopic.findByIdAndDelete(subtopicId);
 
-    res.json({ success: true, message: "Subtopic deleted" });
+    res.json({ success: true });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
   }
@@ -30,12 +29,7 @@ export async function deleteSubtopic(req, res) {
 
 export async function updateSubtopic(req, res) {
   try {
-    const updated = await Subtopic.findByIdAndUpdate(
-      req.params.subtopicId,
-      req.body,
-      { new: true }
-    );
-
+    const updated = await Subtopic.findByIdAndUpdate(req.params.subtopicId, req.body, { new: true });
     res.json({ success: true, subtopic: updated });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
