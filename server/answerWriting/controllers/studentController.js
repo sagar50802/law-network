5ae368f8e182aa25a9d7145657ef0a827e68422b
sagar50802 <sagartripathi1311@gmail.com>
2025-12-07 08:02:36 +0530

    Update studentController.js

--- server/answerWriting/controllers/studentController.js
+++ server/answerWriting/controllers/studentController.js
@@ -1,47 +1,39 @@
+// server/answerWriting/controllers/studentController.js
 import Question from "../models/Question.js";
-import Unit from "../models/Unit.js";
-import Topic from "../models/Topic.js";
-import Subtopic from "../models/Subtopic.js";
 
-const studentController = {
-  async getDashboard(req, res) {
-    try {
-      const examId = req.params.examId;
+export async function getDashboard(req, res) {
+  try {
+    const examId = req.params.examId;
 
-      // Fetch released questions
-      const released = await Question.find({
-        examId,
-        isReleased: true,
-      });
+    const totalReleased = await Question.countDocuments({
+      examId,
+      isReleased: true,
+    });
 
-      res.json({
-        success: true,
-        progress: {
-          total: released.length,
-        },
-      });
-    } catch (err) {
-      res.status(500).json({ success: false, message: err.message });
-    }
-  },
+    res.json({
+      success: true,
+      progress: {
+        totalReleased,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
 
-  async getLiveQuestion(req, res) {
-    try {
-      const now = new Date();
-      const examId = req.params.examId;
+export async function getLiveQuestion(req, res) {
+  try {
+    const examId = req.params.examId;
 
-      const q = await Question.findOne({
-        examId,
-        isReleased: true,
-      })
-        .sort({ releaseAt: -1 })
-        .limit(1);
+    const q = await Question.findOne({
+      examId,
+      isReleased: true,
+    })
+      .sort({ releaseAt: -1 })
+      .limit(1);
 
-      res.json({ success: true, question: q || null });
-    } catch (err) {
-      res.status(500).json({ success: false, message: err.message });
-    }
-  },
-};
-
-export default studentController;
+    res.json({ success: true, question: q || null });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
