3e4cf0f3e6cac4d4ba7173c248a078161b42bd14
sagar50802 <sagartripathi1311@gmail.com>
2025-12-07 07:58:41 +0530

    Update Unit.js

--- server/answerWriting/models/Unit.js
+++ server/answerWriting/models/Unit.js
@@ -1,12 +1,18 @@
+// server/answerWriting/models/Unit.js
 import mongoose from "mongoose";
 
-const UnitSchema = new mongoose.Schema(
+const unitSchema = new mongoose.Schema(
   {
-    examId: { type: mongoose.Schema.Types.ObjectId, ref: "AnswerWritingExam", required: true },
-    name: String,
+    examId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Exam",
+      required: true,
+    },
+    name: { type: String, required: true },
+    locked: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
 
-const Unit = mongoose.model("AnswerWritingUnit", UnitSchema);
+const Unit = mongoose.model("Unit", unitSchema);
 export default Unit;
