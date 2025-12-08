395c8af79e753d7805fe42028044b4f38dc69c47
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 13:28:23 +0530

    Update topicController.js

--- server/answerWriting/controllers/topicController.js
+++ server/answerWriting/controllers/topicController.js
@@ -1,5 +1,5 @@
-import Topic from "../models/Topic.js";
-import Unit from "../models/Unit.js";
+import Topic from "../answerWriting/models/Topic.js";
+import Unit from "../answerWriting/models/Unit.js";
 
 export const createTopic = async (req, res) => {
   try {
