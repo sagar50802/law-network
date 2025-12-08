2b72b891f688b32fdb658c968523fa8e731bd598
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 12:26:43 +0530

    Update Subtopic.js

--- server/answerWriting/models/Subtopic.js
+++ server/answerWriting/models/Subtopic.js
@@ -2,20 +2,8 @@ import mongoose from "mongoose";
 
 const subtopicSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-
-    topic: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Topic",
-      required: true,
-    },
-
-    questions: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Question",
-      },
-    ],
+    topicId: { type: mongoose.Schema.Types.ObjectId, ref: "Topic", required: true },
+    name: { type: String, required: true, trim: true },
   },
   { timestamps: true }
 );
