4e07b5286509c26ff4bf10ad56235a9d94dd4818
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 13:57:17 +0530

    Update topicController.js

--- server/answerWriting/controllers/topicController.js
+++ server/answerWriting/controllers/topicController.js
@@ -1,40 +1,18 @@
 import Topic from "../models/Topic.js";
 import Unit from "../models/Unit.js";
+
 export const createTopic = async (req, res) => {
   try {
     const topic = await Topic.create({
-      unit: req.params.unitId,
       name: req.body.name,
+      unit: req.params.unitId
     });
 
     await Unit.findByIdAndUpdate(req.params.unitId, {
-      $push: { topics: topic._id },
+      $push: { topics: topic._id }
     });
 
     res.json({ success: true, topic });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-};
-
-export const updateTopic = async (req, res) => {
-  try {
-    const topic = await Topic.findByIdAndUpdate(
-      req.params.topicId,
-      { name: req.body.name },
-      { new: true }
-    );
-
-    res.json({ success: true, topic });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-};
-
-export const deleteTopic = async (req, res) => {
-  try {
-    await Topic.findByIdAndDelete(req.params.topicId);
-    res.json({ success: true });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
   }
