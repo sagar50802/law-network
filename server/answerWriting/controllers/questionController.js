3887d36f4e20c650c17f05817cb6cdf02cb36408
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 13:57:54 +0530

    Update questionController.js

--- server/answerWriting/controllers/questionController.js
+++ server/answerWriting/controllers/questionController.js
@@ -7,40 +7,14 @@ export const createQuestion = async (req, res) => {
       unit: req.body.unit,
       topic: req.body.topic,
       subtopic: req.body.subtopic || null,
-
       questionHindi: req.body.questionHindi,
       questionEnglish: req.body.questionEnglish,
       answerHindi: req.body.answerHindi,
       answerEnglish: req.body.answerEnglish,
-
-      releaseTime: req.body.releaseTime,
+      releaseAt: req.body.releaseAt
     });
 
     res.json({ success: true, question: q });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-};
-
-export const deleteQuestion = async (req, res) => {
-  try {
-    await Question.findByIdAndDelete(req.params.questionId);
-    res.json({ success: true });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-};
-
-export const getReleasedQuestions = async (req, res) => {
-  try {
-    const now = new Date();
-
-    const questions = await Question.find({
-      exam: req.params.examId,
-      releaseTime: { $lte: now },
-    }).sort({ releaseTime: 1 });
-
-    res.json({ success: true, questions });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
   }
