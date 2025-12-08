918ca21554f50996f09e2421ecd9c6e94bc085ea
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 23:00:29 +0530

    Create Unit.js

---
+++ server/questionanswer/models/Unit.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const unitSchema = new mongoose.Schema({
+  examId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Exam',
+    required: true
+  },
+  order: {
+    type: Number,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  nameHindi: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  totalTopics: {
+    type: Number,
+    default: 0
+  },
+  totalQuestions: {
+    type: Number,
+    default: 0
+  },
+  completedQuestions: {
+    type: Number,
+    default: 0
+  },
+  isLocked: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+unitSchema.virtual('topics', {
+  ref: 'Topic',
+  localField: '_id',
+  foreignField: 'unitId'
+});
+
+module.exports = mongoose.model('Unit', unitSchema);
