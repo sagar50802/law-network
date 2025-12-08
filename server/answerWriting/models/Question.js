c198b0745d4f56fdd36ab45483022837c533e1e0
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 13:56:01 +0530

    Update Question.js

--- server/answerWriting/models/Question.js
+++ server/answerWriting/models/Question.js
@@ -1,20 +1,17 @@
 import mongoose from "mongoose";
 
-const questionSchema = new mongoose.Schema(
-  {
-    questionHindi: { type: String, required: true },
-    questionEnglish: { type: String, required: true },
-    answerHindi: { type: String, required: true },
-    answerEnglish: { type: String, required: true },
+const questionSchema = new mongoose.Schema({
+  exam: { type: mongoose.Schema.Types.ObjectId, ref: "Exam", required: true },
+  unit: { type: mongoose.Schema.Types.ObjectId, ref: "Unit", required: true },
+  topic: { type: mongoose.Schema.Types.ObjectId, ref: "Topic", required: true },
+  subtopic: { type: mongoose.Schema.Types.ObjectId, ref: "Subtopic" },
 
-    releaseTime: { type: Date, required: true },
+  questionHindi: String,
+  questionEnglish: String,
+  answerHindi: String,
+  answerEnglish: String,
 
-    exam: { type: mongoose.Schema.Types.ObjectId, ref: "Exam", required: true },
-    unit: { type: mongoose.Schema.Types.ObjectId, ref: "Unit" },
-    topic: { type: mongoose.Schema.Types.ObjectId, ref: "Topic" },
-    subtopic: { type: mongoose.Schema.Types.ObjectId, ref: "Subtopic" }
-  },
-  { timestamps: true }
-);
+  releaseAt: { type: Date, required: true }
+});
 
-export default mongoose.models.Question || mongoose.model("Question", questionSchema);
+export default mongoose.model("Question", questionSchema);
