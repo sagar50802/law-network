c394bf009eef7432fbb622f0513d625b83558ef7
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 13:56:25 +0530

    Update examController.js

--- server/answerWriting/controllers/examController.js
+++ server/answerWriting/controllers/examController.js
@@ -1,8 +1,9 @@
 import Exam from "../models/Exam.js";
 import Unit from "../models/Unit.js";
+
 export const createExam = async (req, res) => {
   try {
-    const exam = await Exam.create({ name: req.body.name, units: [] });
+    const exam = await Exam.create({ name: req.body.name });
     res.json({ success: true, exam });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
@@ -25,10 +26,8 @@ export const getExamDetail = async (req, res) => {
         path: "units",
         populate: {
           path: "topics",
-          populate: {
-            path: "subtopics",
-          },
-        },
+          populate: { path: "subtopics" }
+        }
       });
 
     res.json({ success: true, exam });
