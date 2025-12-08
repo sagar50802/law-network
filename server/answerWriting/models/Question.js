2bfb51de642fca83cca2db575992c838c1c7ba43
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 12:27:05 +0530

    Update Question.js

--- server/answerWriting/models/Question.js
+++ server/answerWriting/models/Question.js
@@ -2,37 +2,17 @@ import mongoose from "mongoose";
 
 const questionSchema = new mongoose.Schema(
   {
+    examId: { type: mongoose.Schema.Types.ObjectId, ref: "Exam", required: true },
+    topicId: { type: mongoose.Schema.Types.ObjectId, ref: "Topic" },
+    subtopicId: { type: mongoose.Schema.Types.ObjectId, ref: "Subtopic" },
+
     questionHindi: { type: String, required: true },
     questionEnglish: { type: String, required: true },
 
     answerHindi: { type: String, required: true },
     answerEnglish: { type: String, required: true },
 
-    releaseAt: { type: Date, required: true },
-
-    exam: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Exam",
-      required: true,
-    },
-
-    unit: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Unit",
-      required: true,
-    },
-
-    topic: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Topic",
-      required: true,
-    },
-
-    subtopic: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Subtopic",
-      required: true,
-    },
+    releaseTime: { type: Date, required: true },
   },
   { timestamps: true }
 );
