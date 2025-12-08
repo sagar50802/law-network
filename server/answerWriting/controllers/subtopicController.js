717d1598f32b1b17189f344f88b53d89f4b4def3
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 13:57:36 +0530

    Update subtopicController.js

--- server/answerWriting/controllers/subtopicController.js
+++ server/answerWriting/controllers/subtopicController.js
@@ -5,37 +5,14 @@ export const createSubtopic = async (req, res) => {
   try {
     const sub = await Subtopic.create({
       topic: req.params.topicId,
-      name: req.body.name,
+      name: req.body.name
     });
 
     await Topic.findByIdAndUpdate(req.params.topicId, {
-      $push: { subtopics: sub._id },
+      $push: { subtopics: sub._id }
     });
 
     res.json({ success: true, subtopic: sub });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-};
-
-export const updateSubtopic = async (req, res) => {
-  try {
-    const sub = await Subtopic.findByIdAndUpdate(
-      req.params.subtopicId,
-      { name: req.body.name },
-      { new: true }
-    );
-
-    res.json({ success: true, subtopic: sub });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-};
-
-export const deleteSubtopic = async (req, res) => {
-  try {
-    await Subtopic.findByIdAndDelete(req.params.subtopicId);
-    res.json({ success: true });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
   }
