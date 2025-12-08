41243d8f45f49e6ef9ccd8951eed7bba09bd3f3a
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 13:58:26 +0530

    Update answerWritingRoutes.js

--- server/answerWriting/routes/answerWritingRoutes.js
+++ server/answerWriting/routes/answerWritingRoutes.js
@@ -1,62 +1,42 @@
-// answerWriting/routes/answerWritingRoutes.js
 import express from "express";
+
 import {
-  createExam,
-  getExams,
-  getExamDetail,
+  createExam, getExams, getExamDetail
 } from "../controllers/examController.js";
 
 import {
-  createUnit,
-  deleteUnit,
-  updateUnit,
+  createUnit
 } from "../controllers/unitController.js";
 
 import {
-  createTopic,
-  deleteTopic,
-  updateTopic,
+  createTopic
 } from "../controllers/topicController.js";
 
 import {
-  createSubtopic,
-  deleteSubtopic,
-  updateSubtopic,
+  createSubtopic
 } from "../controllers/subtopicController.js";
 
 import {
-  createQuestion,
-  deleteQuestion,
-  getReleasedQuestions,
+  createQuestion
 } from "../controllers/questionController.js";
 
 const router = express.Router();
 
-/* ---------------- EXAMS ---------------- */
+/* Exams */
 router.post("/exams", createExam);
 router.get("/exams", getExams);
 router.get("/exams/:examId", getExamDetail);
 
-/* ---------------- UNITS ---------------- */
-router.post("/units/:examId", createUnit);
-router.put("/units/:unitId", updateUnit);
-router.delete("/units/:unitId", deleteUnit);
+/* Units */
+router.post("/exams/:examId/units", createUnit);
 
-/* ---------------- TOPICS ---------------- */
-router.post("/topics/:unitId", createTopic);
-router.put("/topics/:topicId", updateTopic);
-router.delete("/topics/:topicId", deleteTopic);
+/* Topics */
+router.post("/units/:unitId/topics", createTopic);
 
-/* ---------------- SUBTOPICS ---------------- */
-router.post("/subtopics/:topicId", createSubtopic);
-router.put("/subtopics/:subtopicId", updateSubtopic);
-router.delete("/subtopics/:subtopicId", deleteSubtopic);
+/* Subtopics */
+router.post("/topics/:topicId/subtopics", createSubtopic);
 
-/* ---------------- QUESTIONS ---------------- */
-router.post("/questions/:examId", createQuestion);
-router.delete("/questions/:questionId", deleteQuestion);
-
-/* ---- Students Only See Released Questions ---- */
-router.get("/released/:examId", getReleasedQuestions);
+/* Questions */
+router.post("/exams/:examId/questions", createQuestion);
 
 export default router;
