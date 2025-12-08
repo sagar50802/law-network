c8f26dcb561e4afe48d94903981900d97d3c2cf2
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 09:08:17 +0530

    Update Subtopic.js

--- server/answerWriting/models/Subtopic.js
+++ server/answerWriting/models/Subtopic.js
@@ -1,11 +1,42 @@
+// server/answerWriting/models/Subtopic.js
 import mongoose from "mongoose";
 
-const SubtopicSchema = new mongoose.Schema(
+const { Schema } = mongoose;
+
+const SubtopicSchema = new Schema(
   {
-    name: { type: String, required: true },
-    topic: { type: mongoose.Schema.Types.ObjectId, ref: "Topic", required: true },
+    examId: {
+      type: Schema.Types.ObjectId,
+      ref: "AnswerWritingExam",
+      required: true,
+    },
+    unitId: {
+      type: Schema.Types.ObjectId,
+      ref: "AnswerWritingUnit",
+      required: true,
+    },
+    topicId: {
+      type: Schema.Types.ObjectId,
+      ref: "AnswerWritingTopic",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    order: {
+      type: Number,
+      default: 0,
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
-export default mongoose.models.Subtopic || mongoose.model("Subtopic", SubtopicSchema);
+const Subtopic =
+  mongoose.models.AnswerWritingSubtopic ||
+  mongoose.model("AnswerWritingSubtopic", SubtopicSchema);
+
+export default Subtopic;
