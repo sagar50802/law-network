c13135998f0aa6525ade12b4b643deff96192743
sagar50802 <sagartripathi1311@gmail.com>
2025-12-08 23:01:31 +0530

    Create Subtopic.js

---
+++ server/questionanswer/models/Subtopic.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const subtopicSchema = new mongoose.Schema({
+  topicId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Topic',
+    required: true
+  },
+  order: {
+    type: Number,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  nameHindi: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  totalQuestions: {
+    type: Number,
+    default: 0
+  },
+  completedQuestions: {
+    type: Number,
+    default: 0
+  },
+  isLocked: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+subtopicSchema.virtual('questions', {
+  ref: 'Question',
+  localField: '_id',
+  foreignField: 'subtopicId'
+});
+
+module.exports = mongoose.model('Subtopic', subtopicSchema);
