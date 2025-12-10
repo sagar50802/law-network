17d3a04c73ac4b278cb8cbbc7590615f7770065b
sagar50802 <sagartripathi1311@gmail.com>
2025-12-10 22:34:34 +0530

    Update topics.js

--- server/routes/qna/topics.js
+++ server/routes/qna/topics.js
@@ -1,30 +1,49 @@
-const express = require("express");
+import express from "express";
+import Topic from "../../models/qna/Topic.js";
+
 const router = express.Router();
-const ownerCheck = require("../../middleware/ownerCheck");
 
-const Topic = require("../../models/qna/Topic");
-
-router.get("/:unitId/topics", async (req, res) => {
-  const topics = await Topic.find({ unitId: req.params.unitId }).sort({ order: 1 });
-  res.json(topics);
+/* GET topics for unit */
+router.get("/units/:unitId/topics", async (req, res) => {
+  try {
+    const topics = await Topic.find({ unitId: req.params.unitId }).sort({ order: 1 });
+    res.json(topics);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-router.post("/:unitId/topics", ownerCheck, async (req, res) => {
-  const topic = await Topic.create({
-    unitId: req.params.unitId,
-    name: req.body.name
-  });
-  res.json(topic);
+/* POST create topic */
+router.post("/units/:unitId/topics", async (req, res) => {
+  try {
+    const topic = await Topic.create({
+      unitId: req.params.unitId,
+      name: req.body.name
+    });
+    res.status(201).json(topic);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
-router.put("/:id", ownerCheck, async (req, res) => {
-  const updated = await Topic.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(updated);
+/* PUT update topic */
+router.put("/topics/:id", async (req, res) => {
+  try {
+    const topic = await Topic.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(topic);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
-router.delete("/:id", ownerCheck, async (req, res) => {
-  await Topic.findByIdAndDelete(req.params.id);
-  res.json({ success: true });
+/* DELETE topic */
+router.delete("/topics/:id", async (req, res) => {
+  try {
+    await Topic.findByIdAndDelete(req.params.id);
+    res.json({ message: "Topic deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-module.exports = router;
+export default router;
