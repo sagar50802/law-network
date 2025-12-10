4e60672a9556ba2cad585c313e4e41ea6af7cbf6
sagar50802 <sagartripathi1311@gmail.com>
2025-12-10 22:58:57 +0530

    Update Subtopic.js

--- server/models/qna/Subtopic.js
+++ server/models/qna/Subtopic.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
-const subtopicSchema = new mongoose.Schema(
+const SubtopicSchema = new mongoose.Schema(
   {
     topicId: { type: mongoose.Schema.Types.ObjectId, ref: "Topic", required: true },
     name: { type: String, required: true },
-    order: { type: Number, default: 0 }
   },
   { timestamps: true }
 );
 
-const Subtopic = mongoose.model("Subtopic", subtopicSchema);
-export default Subtopic;
+export default mongoose.models.Subtopic || mongoose.model("Subtopic", SubtopicSchema);
