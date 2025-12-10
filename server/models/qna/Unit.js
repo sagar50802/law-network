0448b33b61052061a06e5e31bb92cd0e052f24dd
sagar50802 <sagartripathi1311@gmail.com>
2025-12-10 22:58:13 +0530

    Update Unit.js

--- server/models/qna/Unit.js
+++ server/models/qna/Unit.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
-const unitSchema = new mongoose.Schema(
+const UnitSchema = new mongoose.Schema(
   {
     examId: { type: mongoose.Schema.Types.ObjectId, ref: "Exam", required: true },
     name: { type: String, required: true },
-    order: { type: Number, default: 0 }
   },
   { timestamps: true }
 );
 
-const Unit = mongoose.model("Unit", unitSchema);
-export default Unit;
+export default mongoose.models.Unit || mongoose.model("Unit", UnitSchema);
