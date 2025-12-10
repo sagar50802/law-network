8abf4741935bf0a743dadd3ced1252aef33286d2
sagar50802 <sagartripathi1311@gmail.com>
2025-12-10 22:59:22 +0530

    Update Question.js

--- server/models/qna/Question.js
+++ server/models/qna/Question.js
@@ -1,18 +1,14 @@
 import mongoose from "mongoose";
 
-const questionSchema = new mongoose.Schema(
+const QuestionSchema = new mongoose.Schema(
   {
     subtopicId: { type: mongoose.Schema.Types.ObjectId, ref: "Subtopic", required: true },
-
     questionText: { type: String, required: true },
     answerText: { type: String, required: true },
-
     releaseAt: { type: Date, required: true },
-
-    order: { type: Number, default: 0 }
+    order: Number,
   },
   { timestamps: true }
 );
 
-const Question = mongoose.model("Question", questionSchema);
-export default Question;
+export default mongoose.models.Question || mongoose.model("Question", QuestionSchema);
